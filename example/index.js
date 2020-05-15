0ef5af7540668c2b2b7a3b2210ea373e281bac95
Akseli Palen <akseli.palen@gmail.com>
2020-05-15 13:46:50 +0300

    Implement emoji input

--- example/index.js
+++ example/index.js
@@ -4,7 +4,11 @@ require('./style.css')
 require('file-loader?name=[name].[ext]!./favicon.ico')
 require('file-loader?name=[name].[ext]!./index.html')
 
-const container = document.getElementById('container')
-container.appendChild(openmojiPicker.create(emojiEvent => {
+const messageInput = document.getElementById('message-input')
+const pickerContainer = document.getElementById('picker-container')
+
+pickerContainer.appendChild(openmojiPicker.create(emojiEvent => {
   console.log(emojiEvent.hexcode)
+  const emoji = emojiEvent.emoji
+  messageInput.value = messageInput.value + emoji
 }))
